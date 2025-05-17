5c5879b9428415edfb017e55defeac29604bfee2
Reza Saadati <rezasaadati2004@gmail.com>
2025-05-17 12:44:10 +0330

    up to date

--- footer.php
+++ footer.php
@@ -3,8 +3,14 @@ if (!defined('ABSPATH'))
     exit;
 ?>
 <footer>
-    <div class="footer-container">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - All Rights Reserved.</p>
+    <div class="container">
+        <div class = "footer-main">
+            <div class = "col1">test1</div>
+            <div class = "col2">test2</div>
+            <div class = "col3">test3</div>
+            <div class = "col4">test4</div>
+</div>
+        <p>&c4py; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - All Rights Reserved.</p>
         elixweb
     </div>
 </footer>
