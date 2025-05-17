8dcd5694f32727488af5d801dfeb215770a2beb1
Reza Saadati <rezasaadati2004@gmail.com>
2025-05-17 12:50:42 +0330

    Merge branch 'main' of https://github.com/RezaSaadati1/tessstt22

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@ if (!defined('ABSPATH'))
 <footer>
     <div class="footer-container">
         <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - All Rights Reserved.</p>
-        elixweb
+        peyman
     </div>
 </footer>
 <?php wp_footer(); ?>
