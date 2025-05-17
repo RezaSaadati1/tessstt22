f240a27b5d6cb070f2ed08944ce9919a5555ba57
Reza Saadati <rezasaadati2004@gmail.com>
2025-05-17 13:05:11 +0330

    change33

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH'))
             <div class = "col1">test1</div>
             <div class = "col2">test2</div>
             <div class = "col3">test3</div>
-            <div class = "col4">test4</div>
+            <div class = "col4">test5</div>
 </div>
         <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - All Rights Reserved.</p>
         peyman
