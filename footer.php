3e687455b8b6c6ad1b2f9bd92cb99563818699ae
Reza Saadati <rezasaadati2004@gmail.com>
2025-05-17 12:51:30 +0330

    change footer

--- footer.php
+++ footer.php
@@ -3,7 +3,13 @@ if (!defined('ABSPATH'))
     exit;
 ?>
 <footer>
-    <div class="footer-container">
+    <div class="container">
+           <div class = "footer-main">
+            <div class = "col1">test1</div>
+            <div class = "col2">test2</div>
+            <div class = "col3">test3</div>
+            <div class = "col4">test4</div>
+</div>
         <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - All Rights Reserved.</p>
         peyman
     </div>
